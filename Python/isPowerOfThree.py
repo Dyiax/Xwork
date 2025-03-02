fd42148052451f880c897c050715fde2d9853684
Dyiax <2079198609@qq.com>
2025-03-02 23:52:58 +0800

    2025.3.2,23.50 dormitory

---
+++ Python/isPowerOfThree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 0:
+            return False
+        while (n > 1):
+            if (n % 3 != 0):
+                return False
+            n /= 3
+        return True
+
+def main():
+    solution = Solution()
+    print(solution.isPowerOfThree(27))
+
+if __name__ == "__main__":
+    main()
+
+        
